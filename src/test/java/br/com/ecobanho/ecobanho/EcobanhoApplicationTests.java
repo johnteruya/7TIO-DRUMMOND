263beee6520c4317ee53fca4612af2accc9539ff
johnteruya <jteruya9@gmail.com>
2019-04-11 16:23:08 -0300

    iniciando projeto

---
+++ src/test/java/br/com/ecobanho/ecobanho/EcobanhoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ecobanho.ecobanho;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcobanhoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
