5d7d3f45ff998de50130fd96a6a36dbdbe33fb1d
johnteruya <jteruya9@gmail.com>
2019-04-23 17:43:37 -0300

    alterando modelo DB

--- src/main/resources/DB_TEST.sql
+++ src/main/resources/DB_TEST.sql
@@ -2,18 +2,13 @@ CREATE DATABASE ecobanhoapp;
 USE ecobanhoapp;
 DROP DATABASE ecobanho_app;
 
+/* CRIANDO TABELAS */
+
 CREATE TABLE CHUVEIRO(
 	ID_CHUVEIRO int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	TIPO_CHUVEIRO VARCHAR(255) NOT NULL,
 	VAZAO int NOT NULL
 );
-
-insert into chuveiro values (1,'ducha','123');
-insert into chuveiro values (2,'eletrico','456');
-
-delete from chuveiro
-	where id_chuveiro = 1;
-
 
 CREATE TABLE GRUPO(
 	ID_GRUPO int NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -21,33 +16,18 @@ CREATE TABLE GRUPO(
 	NUM_INTEGRANTES int NOT NULL	
 );
 
-insert into grupo values (1, 'teste', 4);
-	UPDATE `grupo` SET `NOME_GRUPO` = 'Silva' WHERE `grupo`.`ID_GRUPO` = 1;
-insert into grupo values (2, 'Santos', 3);
-
-
 CREATE TABLE USUARIO(
 	ID_USUARIO int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	NICKNAME VARCHAR(255) NOT NULL,
 	NOME_USUARIO VARCHAR(255) NOT NULL,
 	SEXO BOOLEAN NOT NULL,
-	TEMPO_USAL int NOT NULL,
+	TEMPO_USUAL int NOT NULL,
 	FK_TIPO_CHUVEIRO int NOT NULL,
 	FK_ID_GRUPO int,
 
 	FOREIGN KEY (FK_TIPO_CHUVEIRO) REFERENCES CHUVEIRO(ID_CHUVEIRO),
 	FOREIGN KEY (FK_ID_GRUPO) REFERENCES GRUPO(ID_GRUPO)
 );
-
-insert into usuario values (1, 'NickTeste', 'marcos antonio', 0, 600, 1, 1);
-	UPDATE `usuario` SET `NOME_USUARIO` = 'Marcos Antonio' WHERE `usuario`.`ID_USUARIO` = 1;
-
-insert into usuario values (2, 'TestandoNick', 'Antonio Marcos', 0, 700, 2, 2);
-
-
-delete from usuario
-	where ID_USUARIO = 1;
-
 
 CREATE TABLE BANHO(
 	ID_BANHO int NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -64,12 +44,60 @@ CREATE TABLE BANHO(
 	FOREIGN KEY(FK_CHUVEIRO) REFERENCES CHUVEIRO(ID_CHUVEIRO)
 );
 
-insert into banho values (1, '2019-01-01', 800, 123, 600, 123, 10, 1, 1);
-insert into banho values (2, '2019-02-02', 700, 456, 600, 465, 15, 2, 2);
+
+/* INSERTS DAS TABELAS */
+
+/* INSERT CHUVEIRO */
+
+INSERT INTO chuveiro (tipo_chuveiro, vazao) VALUES ('DUCHA', '25');
+INSERT INTO chuveiro (tipo_chuveiro, vazao) VALUES ('ELÉTRICO', '15');
 
 
+/* INSERT GRUPO */
+
+INSERT INTO grupo (nome_grupo, num_integrantes) VALUES ('SILVA', 3);
+INSERT INTO grupo (nome_grupo, num_integrantes) VALUES ('MARTINS', 4);
 
 
+/* INSERT USUARIO */
+
+INSERT INTO usuario (nickname, nome_usuario, sexo, tempo_usual, fk_tipo_chuveiro, fk_id_grupo) VALUES ('nick1', 'marcos', 'masculino', 600, 1, 1);
+INSERT INTO usuario (nickname, nome_usuario, sexo, tempo_usual, fk_tipo_chuveiro, fk_id_grupo) VALUES ('nick2', 'antonio', 'masculino', 700, 2, 2);
+INSERT INTO usuario (nickname, nome_usuario, sexo, tempo_usual, fk_tipo_chuveiro, fk_id_grupo) VALUES ('nick3', 'vinicius', 'masculino', 900, 1, 1);
+INSERT INTO usuario (nickname, nome_usuario, sexo, tempo_usual, fk_tipo_chuveiro, fk_id_grupo) VALUES ('nick4', 'maria', 'feminino', 600, 1, 1);
+INSERT INTO usuario (nickname, nome_usuario, sexo, tempo_usual, fk_tipo_chuveiro, fk_id_grupo) VALUES ('nick5', 'graça', 'feminino', 700, 2, 2);
+INSERT INTO usuario (nickname, nome_usuario, sexo, tempo_usual, fk_tipo_chuveiro, fk_id_grupo) VALUES ('nick6', 'marcela', 'feminino', 800, 2, 2);
+INSERT INTO usuario (nickname, nome_usuario, sexo, tempo_usual, fk_tipo_chuveiro, fk_id_grupo) VALUES ('nick7', 'janaina', 'feminino', 900, 2, 2);
+
+
+/* INSERTS USUARIOS */
+
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-15', 600, 9000, 720, 10800, 1800, 1, 2);
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 700, 10500, 720, 10800, 300, 1, 2);
+
+
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-15', 800, 12000, 720, 10800, -1200, 2, 2);
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 600, 9000, 720, 10800, 1800, 2, 2);
+
+
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-15', 700, 10500, 720, 10800, 300, 3, 2);
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 800, 12000, 720, 10800, -1200, 3, 2);
+
+
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-15', 600, 9000, 720, 10800, 1800, 4, 2);
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 700, 10500, 720, 10800, 300, 4, 2);
+
+
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-15', 800, 20000, 720, 18000, -2000, 5, 1);
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 600, 15000, 720, 18000, 3000, 5, 1);
+
+
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 700, 17500, 720, 18000, 500, 6, 1);
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-15', 800, 20000, 720, 18000, -2000, 6, 1);
+
+
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 600, 15000, 720, 18000, 3000, 7, 1);
+insert into banho (data_banho, tempo_real, consumo_real, tempo_ideal, consumo_ideal, economia,fk_usuario,fk_chuveiro) values ('2019-04-16', 700, 17500, 720, 18000, 500, 7, 1);
 
 
 
