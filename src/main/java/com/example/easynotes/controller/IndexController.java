6f2fcbaccc1e28d4d2047daaf8bb1fc723889fc2
johnteruya <jteruya9@gmail.com>
2019-04-23 18:11:50 -0300

    ok

--- src/main/java/com/example/easynotes/controller/IndexController.java
+++ src/main/java/com/example/easynotes/controller/IndexController.java
@@ -1,4 +1,5 @@
-package br.com.ecobanhoapp.controllers;
+package com.example.easynotes.controller;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
