e12d30b9c3990ab2b4a227b20ad5e0b0a4902f52
johnteruya <jteruya9@gmail.com>
2019-05-19 16:11:26 -0300

    ok

--- src/main/java/br/com/ecobanho/repository/ChuveiroRepository.java
+++ src/main/java/br/com/ecobanho/repository/ChuveiroRepository.java
@@ -10,11 +10,10 @@ import org.springframework.stereotype.Repository;
 public interface ChuveiroRepository extends JpaRepository<Chuveiro, Long> {
 
 
-	@Query(value = "select u.id_chuveiro, u.nome_usuario, u.nickname, b.economia, b.data_banho from Usuario u inner join Banho b on u.id_chuveiro = b.fk_usuario where u.id_usuario = ?1 and data_banho >= ?2 and data_banho <= ?3 order by data_banho")
+	@Query(value = "SELECT u.id_chuveiro, u.nome_usuario, u.nickname, b.economia, b.data_banho FROM Usuario u INNER JOIN Banho b ON u.id_chuveiro = b.fk_usuario WHERE u.id_usuario = ?1 AND data_banho >= ?2 AND data_banho <= ?3 ORDER BY data_banho")
 	
 	List<Chuveiro> getUsuarioById(Long id_usuario);
-	
-//	
+
 //	SELECT u FROM Chuveiro u ORDER BY id_chuveiro
 //	SELECT p FROM Produto p WHERE p.description=?1
 //	select c from Chuveiro c where c.id_chuveiro=?1
